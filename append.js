c027670285c5c56ce35dab85e9b82648fc95b606
D-DF <daviddefreitas0192@gmail.com>
2021-03-25 20:39:43 +0100

    ok

--- append.js
+++ append.js
@@ -1,19 +1,22 @@
-const fs = require('fs');
-const tableau = process.argv
-let fichierAcopier = ""
-let copieDuFichier = tableau.pop(); 
-let test = ""
+const fs      = require('fs');
+const tableau = process.argv;
+let nouveauFichier;
 
-for(let i = 2; i < tableau.length; i++) {
-    fichierAcopier += tableau[i] + ' ';
-    console.log(fichierAcopier)
-    
-    test = fs.appendFileSync(copieDuFichier, fichierAcopier)
+if (tableau.length <= 3) {
+    console.log(`Error: Il n'y a pas assez d'argument.`)
+    process.exit(1)
 }
 
-console.log(test)
+for(let i = 2; i < tableau.length; i++) {
+    const stats = fs.statSync(tableau[i])
 
+    if (!stats.isFile(tableau[i])) {
+    console.log(`Error: Ceci n'est pas un fichier`)
+    process.exit(1)
+    }
 
+    nouveauFichier = fs.readFileSync(tableau[i], 'utf-8')
+    fs.appendFileSync(tableau[tableau.length - 1], nouveauFichier)
+}
 
-
-
+console.log(nouveauFichier)
