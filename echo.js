7320087bdf1f10b39e1b0083f3743cbcca05acbc
D-DF <daviddefreitas0192@gmail.com>
2021-03-25 14:14:09 +0100

    ok

--- echo.js
+++ echo.js
@@ -1,6 +1,11 @@
 let tableau = process.argv;
 let phrase  = '';
 
+if(tableau.length < 3) {
+    console.log(`Error: Il n'y a pas de message a afficher`);
+    process.exit(1);
+}
+
 for(let i = 2 ; i < tableau.length; i++) {
     phrase += tableau[i] + ' ';
 }
