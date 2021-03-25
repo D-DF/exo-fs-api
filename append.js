9316528a06f029db9fa7b702c29a486c98df1f4d
D-DF <daviddefreitas0192@gmail.com>
2021-03-25 17:16:26 +0100

    ok

--- append.js
+++ append.js
@@ -1,2 +1,19 @@
 const fs = require('fs');
+const tableau = process.argv
+let fichierAcopier = ""
+let copieDuFichier = tableau.pop(); 
+let test = ""
+
+for(let i = 2; i < tableau.length; i++) {
+    fichierAcopier += tableau[i] + ' ';
+    console.log(fichierAcopier)
+    
+    test = fs.appendFileSync(copieDuFichier, fichierAcopier)
+}
+
+console.log(test)
+
+
+
+
 
