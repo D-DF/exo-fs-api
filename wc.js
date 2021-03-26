f2d8421e5fdf4d2d533e774c4090309b5ebfb197
D-DF <daviddefreitas0192@gmail.com>
2021-03-26 16:11:04 +0100

    OK

--- wc.js
+++ wc.js
@@ -1,51 +1,75 @@
 const fs = require('fs');
 
-if(process.argv[2] === "-l") {
-    let lireFichie = fs.readFileSync(process.argv[3], 'utf-8')
-    let tableauLign = lireFichie.split('\n');
-    let countLign = tableauLign.length;
-    console.log(countLign + ' ' + process.argv[3])
-}
 
-if(process.argv[2] === "-w") {
-    let lireFichie = fs.readFileSync(process.argv[3], 'utf-8')
-    let tableauLigne = lireFichie.split('\n');
-    let countMot   = 0
+if(process.argv[2] !== "-l" && process.argv[2] !== "-w" && process.argv[2] !== "-c") {
 
-    for(let i = 0; i < tableauLigne.length; i ++) {
-        countMot += i;
+    console.log(`Error: node wc.js -l ou -w ou -c nom du fichier`)
+
+} else {
+    if(process.argv[2] === "-l") {
+        if (!fs.existsSync(process.argv[3])) {
+            console.log(`Désoler ${process.argv[3]} n\'existe pas`)
+            process.exit(1)
+          }
+          
+        if(process.argv.length !== 4) {
+            console.log(`Error: node wc.js -l nom du fichier`)
+            process.exit(1);
+        }
+    
+        let lireFichie = fs.readFileSync(process.argv[3], 'utf-8')
+        let tableauLign = lireFichie.split('\n');
+        let countLign = tableauLign.length;
+        console.log(countLign + ' ' + process.argv[3])
     }
+    
+    if(process.argv[2] === "-w") {
+        if (!fs.existsSync(process.argv[3])) {
+            console.log(`Désoler ${process.argv[3]} n\'existe pas`)
+            process.exit(1)
+          }
 
-    console.log(countMot + ' ' + process.argv[3])
-}
-
-if(process.argv[2] === "-c") {
-    let lireFichie = fs.readFileSync(process.argv[3], 'utf-8')
-    const tableau = lireFichie.split('');
-    let caractere = tableau.length;
-
-    console.log(caractere + ' ' + process.argv[3])
-}
-
-
-if(process.argv.length === 3) {
-    const lireFichier = fs.readFileSync(process.argv[2], 'utf-8')
-
-    let tableauLigne = lireFichier.split('\n');
-    let countLigne = tableauLigne.length;
-    let countMot   = 0
-
-    for(let i = 0; i < tableauLigne.length; i ++) {
-        countMot += i;
+        if(process.argv.length !== 4) {
+            console.log(`Error: node wc.js -w nom du fichier`)
+            process.exit(1);
+        }
+    
+        let lireFichie = fs.readFileSync(process.argv[3], 'utf-8')
+        let tableauLigne = lireFichie.split('\n');
+        let countMot   = 0
+    
+        for(let i = 0; i < tableauLigne.length; i ++) {
+            countMot += i;
+        }
+    
+        console.log(countMot + ' ' + process.argv[3])
     }
+    
+    if(process.argv[2] === "-c") {
 
-    const tableau = lireFichier.split('');
-    let caractere = tableau.length;
+        if (!fs.existsSync(process.argv[3])) {
+            console.log(`Désoler ${process.argv[3]} n\'existe pas`)
+            process.exit(1)
+          }
 
-    let final = `${countLigne} ${countMot} ${caractere} ${process.argv[2]}`
-
-    console.log(final)
+        if(process.argv.length !== 4) {
+            console.log(`Error: node wc.js -c nom du fichier`)
+            process.exit(1);
+        }
+    
+        let lireFichie = fs.readFileSync(process.argv[3], 'utf-8')
+        const tableau = lireFichie.split('');
+        let caractere = tableau.length;
+    
+        console.log(caractere + ' ' + process.argv[3])
+    }
 }
+
+
+
+
+
+
 
 
 
---
+++ wc1.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+
+if (process.argv.length === 3) {
+    if (!fs.existsSync(process.argv[2])) {
+        console.log(`Désoler ${process.argv[2]} n\'existe pas`)
+        process.exit(1)
+      }
+    
+    const lireFichier = fs.readFileSync(process.argv[2], 'utf-8')
+
+    let tableauLigne = lireFichier.split('\n');
+    let countLigne = tableauLigne.length;
+    let countMot   = 0
+
+    for(let i = 0; i < tableauLigne.length; i ++) {
+        countMot += i;
+    }
+
+    const tableau = lireFichier.split('');
+    let caractere = tableau.length;
+
+    let final = `${countLigne} ${countMot} ${caractere} ${process.argv[2]}`
+
+    console.log(final)
+
+} else {
+    console.log(`Error: node wc.js nom du fichier`)
+    process.exit(1);
+}
