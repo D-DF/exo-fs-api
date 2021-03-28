6db62a1a4c99aa406994dd295b7ccc6be7b41475
D-DF <daviddefreitas0192@gmail.com>
2021-03-28 14:27:05 +0200

    OK

--- tail.js
+++ tail.js
@@ -1,19 +1,13 @@
-
 const fs = require('fs');
 
-if (process.argv[2] !== "-n") {
-    const stats = fs.statSync(process.argv[2])
+if (process.argv.length !== 3 && process.argv.length !== 5) {
+    console.log(`Error: node tail.js nom de votre fichier`)
+    process.exit(1);
 
-    if (process.argv.length !== 3) {
-        console.log(`Error: node tail.js nom de votre fichier`)
-        process.exit(1)
-    }
-
-    if (!stats.isFile(process.argv[2])) {
-        console.log(`${process.argv[2]} n\'est pas un fichier`)
-        process.exit(1)
-    }
-
+} else if (!fs.existsSync(process.argv[2]) && !fs.existsSync(process.argv[4])) {
+    console.log(`Désoler ${process.argv[2]} n\'existe pas`)
+    process.exit(1)
+} else if (process.argv.length === 3) {
     // Lire le fichier
     const lireFichier = fs.readFileSync(process.argv[2], 'utf-8');
 
@@ -22,14 +16,12 @@ if (process.argv[2] !== "-n") {
     let resultat = ""
 
     // Récupération des 10dernières lignes du texte.
-    for(let i = tableauFichier.length - 10; i < tableauFichier.length; i ++) {
-            resultat += tableauFichier[i]
+    for (let i = tableauFichier.length - 10; i < tableauFichier.length; i++) {
+        resultat += tableauFichier[i]
     }
 
-    console.log(resultat)
-}
-
-if(process.argv[2] === "-n") {
+    console.log(resultat);
+} else if (process.argv[2] === "-n") {
     const lire = fs.readFileSync(process.argv[4], 'utf-8');
     const tableau = lire.split('\n')
     const recupLigne = process.argv[3];
